6da9f9fd7769d5f3548ca741cf07be8c69ef3e55
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2021-09-18 22:57:24 +0700

    1 st commit

---
+++ matrix1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,m,n,a[3][3],b[3][3];
+    scanf("%d %d",&m,&n);
+    for(i = 0 ; i<m ; i++)
+    {
+        for(j = 0 ; j<n ; j++)
+        {
+            scanf("%d",&a[i][j]);
+            
+        }
+    }
+    for(i = 0 ; i<m ; i++)
+    {
+        for(j = 0 ; j<n ; j++)
+        {
+            scanf("%d",&b[i][j]);
+            
+        }
+    }
+    printf("\n");
+    for(i = 0 ; i<m ; i++)
+    {
+        for(j = 0 ; j<n ; j++)
+        {
+            printf("%d ",a[i][j] + b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0 ;
+}
