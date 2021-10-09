18bd3a2e9ae34339b0c07ed7b2e8f7216a35352b
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2021-10-09 23:27:35 +0700

    2nd commit

--- WP90_2.c
+++ WP90_2.c
@@ -1,15 +1,21 @@
 #include<stdio.h> //*sqrt() don't use math.h
+float sqrt(int num)
+{
+    float x[10000], result=0 ;
+    for(int i = 0 ; i <= 10000 ; i++)
+    {
+        x[0]=1 ;
+        x[i+1] = (x[i] + num / x[i])/2 ; 
+        result = x[10000];
+    }
+     return result;
+}
 int main()
 {
-    int num ;
+    int num;
     printf("Enter number : ");
-    scanf("%d" ,&num);
-    for(int i = 0 ; i < num ; i++)
-    {
-        if(i*i == num)
-        {
-            printf("Result = %d",i);
-        }
-    }
+    scanf("%d" , &num);
+    printf("Result = %f",sqrt(num));
+    return 0;
     return 0;
 }
