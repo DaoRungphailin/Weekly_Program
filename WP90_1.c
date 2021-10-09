f85cc77e1a867cd73aa0f2a8c899d5a67b6897e0
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2021-10-09 23:29:09 +0700

    format doc

--- WP90_1.c
+++ WP90_1.c
@@ -1,16 +1,16 @@
-#include<stdio.h> //*sqrt() don't use math.h
+#include <stdio.h> //*sqrt() don't use math.h
 int main()
 {
     int num;
-    float x[10000], result=0 ;
+    float x[10000], result = 0;
     printf("Enter number : ");
-    scanf("%d" , &num);
-    for(int i = 0 ; i <= 10000 ; i++)
+    scanf("%d", &num);
+    for (int i = 0; i <= 10000; i++)
     {
-        x[0]=1 ;
-        x[i+1] = (x[i] + num / x[i])/2 ; 
+        x[0] = 1;
+        x[i + 1] = (x[i] + num / x[i]) / 2;
         result = x[10000];
     }
-    printf("Result = %f",result);
+    printf("Result = %f", result);
     return 0;
 }
--- WP90_2.c
+++ WP90_2.c
@@ -1,21 +1,21 @@
-#include<stdio.h> //*sqrt() don't use math.h
+#include <stdio.h> //*sqrt() don't use math.h
 float sqrt(int num)
 {
-    float x[10000], result=0 ;
-    for(int i = 0 ; i <= 10000 ; i++)
+    float x[10000], result = 0;
+    for (int i = 0; i <= 10000; i++)
     {
-        x[0]=1 ;
-        x[i+1] = (x[i] + num / x[i])/2 ; 
+        x[0] = 1;
+        x[i + 1] = (x[i] + num / x[i]) / 2;
         result = x[10000];
     }
-     return result;
+    return result;
 }
 int main()
 {
     int num;
     printf("Enter number : ");
-    scanf("%d" , &num);
-    printf("Result = %f",sqrt(num));
+    scanf("%d", &num);
+    printf("Result = %f", sqrt(num));
     return 0;
     return 0;
 }
